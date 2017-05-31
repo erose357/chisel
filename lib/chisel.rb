505b2e72e39c84bae893d7ab01eb6896706699a5
erose357 <erose357@gmail.com>
2017-05-31 13:55:29 -0600

    add all header methods

--- lib/chisel.rb
+++ lib/chisel.rb
@@ -5,4 +5,26 @@ class Chisel
     to_html = "<h1>#{remove_md}</h1>"
   end
 
+  def header_two(chunk)
+    remove_md = (chunk.delete("#")).strip
+    to_html = "<h2>#{remove_md}</h2>"
+  end
+
+  def header_three(chunk)
+    remove_md = (chunk.delete("#")).strip
+    to_html = "<h3>#{remove_md}</h3>"
+  end
+
+  def header_four(chunk)
+    remove_md = (chunk.delete("#")).strip
+    to_html = "<h4>#{remove_md}</h4>"
+  end
+
+  def header_five(chunk)
+    remove_md = (chunk.delete("#")).strip
+    to_html = "<h5>#{remove_md}</h5>"
+  end
+
+
+
 end
--- test/chisel_test.rb
+++ test/chisel_test.rb
@@ -12,9 +12,37 @@ class ChiselTest < Minitest::Test
 
   def test_header_one
     ch = Chisel.new
-    header = "# Header1"
+    header = "# Header 1"
 
-    assert_equal "<h1>Header1</h1>", ch.header_one(header)
+    assert_equal "<h1>Header 1</h1>", ch.header_one(header)
+  end
+
+  def test_header_two
+    ch = Chisel.new
+    header = "## Header 2"
+
+    assert_equal "<h2>Header 2</h2>", ch.header_two(header)
+  end
+
+  def test_header_three
+    ch = Chisel.new
+    header = "### Header 3"
+
+    assert_equal "<h3>Header 3</h3>", ch.header_three(header)
+  end
+
+  def test_header_four
+    ch = Chisel.new
+    header = "#### Header 4"
+
+    assert_equal "<h4>Header 4</h4>", ch.header_four(header)
+  end
+
+  def test_header_five
+    ch = Chisel.new
+    header = "##### Header 5"
+
+    assert_equal "<h5>Header 5</h5>", ch.header_five(header)
   end
 
 end
